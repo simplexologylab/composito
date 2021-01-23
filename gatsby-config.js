c0b6277814c98600d13a2c0063d27758d74073ef
simplexologylab <ramsey@simplexologylab.com>
2021-01-23 09:22:57 -0500

    icon; offline;

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,7 +2,7 @@ module.exports = {
   siteMetadata: {
     title: `com·pò·ṣi·to`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    author: `@simplexologylab`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -42,17 +42,17 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Composito`,
+        short_name: `composito`,
         start_url: `/`,
         background_color: `#B40D1A`,
         theme_color: `#B40D1A`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        display: `standalone`,
+        icon: `src/images/logo.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    `gatsby-plugin-offline`,
   ],
 }
