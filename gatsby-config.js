19c5137599797b6145d4160b2606c4be41f59d01
simplexologylab <ramsey@simplexologylab.com>
2021-02-04 12:28:35 -0500

    update

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,6 +7,7 @@ module.exports = {
   plugins: [
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-styled-components`,
+    `gatsby-plugin-robots-txt`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
